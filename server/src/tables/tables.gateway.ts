b6edb02530f374f3ab33285dfdbfe03b511be011
razorbanana <stanislav.stefyuk2@gmail.com>
2024-08-07 12:55:13 +0300

    feat: socket gateway on connect and disconnect

--- server/src/tables/tables.gateway.ts
+++ server/src/tables/tables.gateway.ts
@@ -1,15 +1,38 @@
 import { Logger } from '@nestjs/common';
-import { OnGatewayInit, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { TablesService } from './tables.service';
+import { Namespace, Socket } from 'socket.io';
 
 @WebSocketGateway({
   namespace: 'tables',
 })
-export class TablesGateway implements OnGatewayInit {
+export class TablesGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   private readonly logger = new Logger(TablesGateway.name);
   constructor(private readonly tablesService: TablesService) {}
 
+  @WebSocketServer() io: Namespace;
+
   afterInit(): void {
     this.logger.log(`Websocket gateway initialized`);
+  }
+
+  handleConnection(client: Socket) {
+    const sockets = this.io.sockets;
+    this.logger.log(`WS Client with id ${client.id} connected`);
+    this.logger.debug(`Number of connected clients: ${sockets.size}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    const sockets = this.io.sockets;
+    this.logger.log(`WS Client with id ${client.id} disconnected`);
+    this.logger.debug(`Number of connected clients: ${sockets.size}`);
   }
 }
